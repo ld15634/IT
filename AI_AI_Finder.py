2f53ceef69cf49914d845c6b969e413ce498a363
Lucio <ld15634p@pace.edu>
2026-06-09 16:00:34 -0400

    Start of Final Project

---
+++ AI_AI_Finder.py
@@ -0,0 +1,3 @@
+print ("Welcome to the AI Finder!")
+print ("This program will help you find the best AI Chatbot for your specific struggles.")
+print ("Please answer the following questions to help us narrow down the options for you.")
